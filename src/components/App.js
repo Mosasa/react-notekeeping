c4ba5ad1a4c386536cab2d986e9f25a620eea2fd
mosasa <1462546799@qq.com>
2018-08-03 15:13:32 +0800

    tab切换 样式响应

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,34 @@ import More from './more/More';
 import { BrowserRouter as Router, Route, Switch, Redirect, NavLink } from 'react-router-dom'//as 将BrowserRouter用router替代
 // import '../assets/stylus/reset.styl';
 // import '../assets/stylus/font.styl';
-import belongings from '../assets/fonts/belongings.png';//将图片作为资源引入
+
+//将图片作为资源引入
+import belongings from '../assets/fonts/belongings.png';
+import belongings1 from '../assets/fonts/belongings1.png';
 import report from '../assets/fonts/report.png';
+import report1 from '../assets/fonts/report1.png';
 import bill from '../assets/fonts/bill.png';
+import bill1 from '../assets/fonts/bill1.png';
 import manageFinances from '../assets/fonts/money.png';
+import manageFinances1 from '../assets/fonts/money1.png';
 import more from '../assets/fonts/more.png';
+import more1 from '../assets/fonts/more1.png';
 import './App.styl';
 
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      markArray: [0, 0, 0, 0, 0],
+    }
+  };
+  activateButton(id) {  
+    //根据参数id，来确定激活五个tab中的哪一个          
+    let tmpMarkArray = [0, 0, 0, 0, 0]             
+    tmpMarkArray[id] = 1;            
+    this.setState({markArray: tmpMarkArray});         
+  }
   render() {
     return (
       <Router>
@@ -36,32 +55,37 @@ class App extends Component {
           </div>
           <footer className="app-footer footer-tab">
             <div className="tab-item">
-              <NavLink to="/belongings" className="nav-link">
-                <img src={ belongings } alt="" className="tab-logo"/> 
+              <NavLink to="/belongings" className="nav-link" onClick = { this.activateButton.bind(this, 0)
+              }>
+                <img src={ (this.state.markArray)[0] ? belongings1 : belongings } alt="" className="tab-logo"/> 
                 <span>资产</span>
               </NavLink>
             </div>
             <div className="tab-item">
-              <NavLink to="/report" className="nav-link">
-                <img src={ report } alt="" className="tab-logo"/> 
-                <span>报表</span>
+              <NavLink to="/report" className="nav-link" onClick = { this.activateButton.bind(this, 1)
+              }>
+                <img src={ (this.state.markArray)[1] ? report1 : report } alt="" className="tab-logo" /> 
+                <span >报表</span>
               </NavLink>
             </div>
             <div className="tab-item selected">
-              <NavLink to="/bill" className="nav-link">
-                <img src={ bill } alt="" className="tab-logo"/> 
-                <span>账单</span>
+              <NavLink to="/bill" className="nav-link" onClick = { this.activateButton.bind(this, 2)
+              }>
+                <img src={ (this.state.markArray)[2] ? bill1 : bill} alt="" className={(this.state.markArray)[2] ? "tab-logo-active":"tab-logo"}/> 
+                <span style={{display: (this.state.markArray)[2] ? 'none' : 'block'}} className="bill">账单</span>
               </NavLink>
             </div>
             <div className="tab-item">
-              <NavLink to="/manageFinances" className="nav-link">
-                <img src={ manageFinances } alt="" className="tab-logo"/> 
+              <NavLink to="/manageFinances" className="nav-link" onClick = { this.activateButton.bind(this, 3)
+              }>
+                <img src={ (this.state.markArray)[3] ? manageFinances1 : manageFinances } alt="" className="tab-logo"/> 
                 <span>理财</span>
               </NavLink>
             </div>
             <div className="tab-item">
-              <NavLink to="/more" className="nav-link">
-                <img src={ more } alt="" className="tab-logo"/> 
+              <NavLink to="/more" className="nav-link" onClick = { this.activateButton.bind(this, 4)
+              }>
+                <img src={ (this.state.markArray)[4] ? more1 : more } alt="" className="tab-logo"/> 
                 <span>更多</span>
               </NavLink>
             </div>
